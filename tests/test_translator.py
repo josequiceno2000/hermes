2f315268e3a2428142c3442976789dd23245a37e
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-04-17 16:16:51 -0500

    test(test_translator): test greek map

--- tests/test_translator.py
+++ tests/test_translator.py
@@ -4,11 +4,27 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 import translator
 
-def test_translate_name_basic():
-    test_map = {
+def test_translate_name_latin():
+    latin_map = {
         "ph": "f",
         "ae": "æ",
         "j": "i"
     }
-    result = translator.translate_name("Jaeph", test_map)
-    assert result == "Iæf"
+    assert translator.translate_name("Jaeph", latin_map) == "Iæf"
+
+def test_translate_name_greek():
+    greek_map = {
+        "ph": "φ",
+        "th": "θ",
+        "ch": "χ",
+        "ps": "ψ",
+        "x": "ξ",
+        "r": "ρ",
+        "y": "υ",
+        "i": "ι",
+        "l": "λ",
+        "s": "ς",
+        "u": "υ",
+        "o": "ο"
+    }
+    assert translator.translate_name("Thorphiulos", greek_map) == "Θορφιυλος"
